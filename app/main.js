b98202ff40bb651ac3d877f9aa30f2edb2bd3e51
prawin10 <prawinaga10@gmail.com>
2021-11-25 20:59:24 +0530

    angular-practise

---
+++ app/main.js
@@ -0,0 +1,135 @@
+var data = [  
+    {
+      "profile": {
+          "picture": "",
+          "name": "",
+          "location": "",
+          "time": ""
+      },    
+      "post": "John is world famous professional photographer. with forward thinking clients to create beautiful, honest and amazing things that bring positive results. John is world famous professional photographer. with forward thinking clients to create beautiful, honest and amazing things that bring positive results." ,
+      "likes": [
+          "John Due", 
+          "Danieal Kalion",
+          "Ramki",
+          "Subash" ,
+          "Ravikumar"
+      ],
+      "comments": [
+        {
+           "picture":"",
+           "name":"",
+           "comment":"",
+           "time":"" 
+        },
+        {
+          "picture":"",
+          "name":"",
+          "comment":"",
+          "time":"" 
+       },
+       {
+          "picture":"",
+          "name":"",
+          "comment":"",
+          "time":"" 
+       },
+       {
+          "picture":"",
+          "name":"",
+          "comment":"",
+          "time":"" 
+       }
+      ]  
+    },
+    {
+      "profile": {
+          "picture": "",
+          "name": "",
+          "location": "",
+          "time": ""
+      },    
+      "post": "John is world famous professional photographer. with forward thinking clients to create beautiful, honest and amazing things that bring positive results. John is world famous professional photographer. with forward thinking clients to create beautiful, honest and amazing things that bring positive results." ,
+      "likes": [
+          "John Due", 
+          "Danieal Kalion",
+          "Ramki",
+          "Subash" ,
+          "Ravikumar"
+      ],
+      "comments": [
+        {
+           "picture":"",
+           "name":"",
+           "comment":"",
+           "time":"" 
+        },
+        {
+          "picture":"",
+          "name":"",
+          "comment":"",
+          "time":"" 
+       },
+       {
+          "picture":"",
+          "name":"",
+          "comment":"",
+          "time":"" 
+       },
+       {
+          "picture":"",
+          "name":"",
+          "comment":"",
+          "time":"" 
+       }
+      ]  
+    },
+    {
+      "profile": {
+          "picture": "",
+          "name": "",
+          "location": "",
+          "time": ""
+      },    
+      "post": "John is world famous professional photographer. with forward thinking clients to create beautiful, honest and amazing things that bring positive results. John is world famous professional photographer. with forward thinking clients to create beautiful, honest and amazing things that bring positive results." ,
+      "likes": [
+          "John Due", 
+          "Danieal Kalion",
+          "Ramki",
+          "Subash" ,
+          "Ravikumar"
+      ],
+      "comments": [
+        {
+           "picture":"",
+           "name":"",
+           "comment":"",
+           "time":"" 
+        },
+        {
+          "picture":"",
+          "name":"",
+          "comment":"",
+          "time":"" 
+       },
+       {
+          "picture":"",
+          "name":"",
+          "comment":"",
+          "time":"" 
+       },
+       {
+          "picture":"",
+          "name":"",
+          "comment":"",
+          "time":"" 
+       }
+      ]  
+    }
+  ]
+
+
+function car(singleData){
+   console.log(singleData)
+}
+
+data.forEach(car)
